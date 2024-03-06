95a1dc216e3244eb5ddfee9d8da7fb21779ea41c
Rick Jen <rickijen@gmail.com>
2024-03-05 22:51:51 -0800

    feat: add API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `<div>Hello {{value}}</div>`,
+// })
+// export class AppComponent {
+//   value = 'World';
+// }
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
+// import { BrowserModule } from "@angular/platform-browser";
+// import { NgModule } from "@angular/core";
+
+// import { AppComponent } from "./app.component";
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   imports: [BrowserModule],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule {}
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
